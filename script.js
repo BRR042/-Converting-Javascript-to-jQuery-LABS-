163cc753352700daa6291901b6dab166c22ab0a5
BRR042 <brrobbins07@gmail.com>
2024-09-22 23:55:31 -0400

    Removed

--- script.js
+++
@@ -1,8 +0,0 @@
-function alertHover(){
-    document.getElementById("area").style.backgroundColor = "green";
-    alert("Close me to turn the box green.");
-  }
-  
-  function reset(){
-     document.getElementById("area").style.backgroundColor = "#953674"; 
-  }
